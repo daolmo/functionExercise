ded792ae6a5949c940ab65ecfa6032232aa0ffcf
daolmo <67968940+daolmo@users.noreply.github.com>
2020-07-20 12:13:02 -0500

    First commit
    First commit

--- Daniel_O_FunctionExercise.playground/Contents.swift
+++ Daniel_O_FunctionExercise.playground/Contents.swift
@@ -1,13 +1,14 @@
 import UIKit
+
 // creating a function to add two numbers
-func addNum(total: Int) -> Int {
+func addNum(numOne: Int, numTwo: Int) -> Int {
 // generating two random numbers and adding them together
-    let randomNumOne = Int.random(in: 1..<100)
-    let randomNumTwo = Int.random(in: 0..<500)
-    let total = randomNumOne + randomNumTwo
+    let numOne = Int.random(in: 1..<100)
+    let numTwo = Int.random(in: 0..<500)
+    let total = numOne + numTwo
+    print("\(numOne) + \(numTwo) = \(total)")
 // returning the total
     return total
 }
-// printing the total (not sure how to print the value with text and without putting a value after total:)
-print(addNum(total: 0))
-
+//calling the function
+addNum(numOne: Int.random(in: 1..<100), numTwo: Int.random(in: 1..<500))
