7998fde383324568aff3c9937b788a4bd6798dd4
daolmo <67968940+daolmo@users.noreply.github.com>
2020-07-20 11:51:59 -0500

    First commit
    First commit

--- Daniel_O_FunctionExercise.playground/Contents.swift
+++ Daniel_O_FunctionExercise.playground/Contents.swift
@@ -1,13 +1,13 @@
 import UIKit
-
+// creating a function to add two numbers
 func addNum(total: Int) -> Int {
-    
+// generating two random numbers and adding them together
     let randomNumOne = Int.random(in: 1..<100)
     let randomNumTwo = Int.random(in: 0..<500)
     let total = randomNumOne + randomNumTwo
-
+// returning the total
     return total
 }
-
+// printing the total (not sure how to print the value with text and without putting a value after total:)
 print(addNum(total: 0))
 
